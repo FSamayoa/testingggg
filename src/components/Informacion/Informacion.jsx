ab371b44cd0dca9d50a2c540f2216f2ba58e0a44
Samayoa <fsamayoal@gmail.com>
2024-02-21 11:19:10 -0600

    fetch

--- src/components/Informacion/Informacion.jsx
+++ src/components/Informacion/Informacion.jsx
@@ -1,9 +1,38 @@
-const Informacion = () => {
-    return (
-        <div>
-            <button>Enviar</button>
-        </div>
-    )
-}
+import React, { useState } from "react";
 
-export default Informacion
+const Informacion = () => {
+  const [data, setData] = useState("3");
+
+  const getData = async () => {
+    try {
+      const response = await fetch("https://ntiirqazzpdwwxqffxss.supabase.co/rest/v1/equipos?select=*",{
+        method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50aWlycWF6enBkd3d4cWZmeHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0NTIwODYsImV4cCI6MjAyNDAyODA4Nn0.mr1JQe6gdgS6Fq5zz53DGXn7Hs8WQuz3RIi7rZTxi0k', // Reemplaza con tu clave de API de Supabase
+                    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50aWlycWF6enBkd3d4cWZmeHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0NTIwODYsImV4cCI6MjAyNDAyODA4Nn0.mr1JQe6gdgS6Fq5zz53DGXn7Hs8WQuz3RIi7rZTxi0k`, // Reemplaza con tu clave de API de Supabase
+      },
+    });
+      if (!response.ok) {
+        throw Error("No se encontraron los datos");
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+        console.error('Error al obtener los datos', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={getData}>Enviar</button>
+      {data && (
+                <div>
+                    {/* Renderiza los datos */}
+                </div>
+            )}
+    </div>
+  );
+};
+
+export default Informacion;
